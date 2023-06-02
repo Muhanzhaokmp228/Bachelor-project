44fbeed1f79f72fee4e23b7500f218155ae4fa7b
Muhan Zhao <Muhan_zhao@hotmail.com>
2023-06-02 16:39:55 +0200

    update projection point to mesh

---
+++ Code/mito.py
@@ -0,0 +1,69 @@
+import numpy as np
+import csv
+import igl
+import math
+import ripleyK as rk
+import ripleyKtest as rkt
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d
+from scipy.io import loadmat 
+import os
+import geopandas as gpd
+import plotly.graph_objects as go
+
+def readmitodata(fname):
+    """
+    Expect a filename of the form "mito_data???.mat" 
+    where ??? is the mito number (left padded by 0 is necessary).
+    
+    argument:
+    fname -- string, filename of the mito data
+    
+    returns:
+    mito_dict -- dictionary, keys are the names of the variables in the matlab file
+    The keys are:
+    'mito' -- the mitochondrion binary shaoe (np.ndarray, has been padded with zeros)
+    'vertices' -- the vertices of the meshed mitochondrion (np.ndarray)  
+    'faces' -- the faces of the meshed mitochondrion (np.ndarray)
+    'cristae_junctions' -- the vertices of the cristae junctions (np.ndarray) or empty list if none
+    'min_coords' -- the minimum coordinates of the mitochondrion (np.ndarray) in the original volume    
+    'mito_number' -- the number of the mitochondrion (int) from the cc3d.component_labeling function,
+                        this is the same as the number in the filename
+    
+    """
+    mito_dict = loadmat(fname)
+    # remove matlab meta data
+    del mito_dict['__header__']
+    del mito_dict['__version__']
+    del mito_dict['__globals__']
+    mito_dict['mito_number'] = int(mito_dict['mito_number'])
+    # inverted results in matlab....
+    mito_dict['vertices'], mito_dict['faces'] = mito_dict['faces'], mito_dict['vertices']
+    # count starts at 0
+    mito_dict['faces'] -= 1
+    return mito_dict
+
+current_dir = os.getcwd()
+print(current_dir)
+data_dir = os.path.join(current_dir, "Code", "mito_data")
+result_dir = os.path.join(current_dir, "Code", "mito_result")
+
+for file_name in os.listdir(data_dir):
+    if file_name.endswith(".mat"):
+        file_path = os.path.join(data_dir, file_name)
+        mito = readmitodata(file_path)
+        vertices = np.array(mito['vertices'], dtype=np.float64)
+        faces = np.array(mito['faces'], dtype=np.int32)
+        cj = np.array(mito['cristae_junction'], dtype=np.float64)
+        points = cj.T
+        rmax = math.sqrt(rk.mesh_area(vertices, faces) / 2)
+        radii = np.linspace(0, rmax, 50)
+        if cj.size:
+            print(file_name)
+            kt_mito = rk.ripleyK_mesh(vertices, faces, points, radii)
+            data = np.column_stack((radii, kt_mito))
+            result_path = os.path.join(result_dir, os.path.splitext(file_name)[0] + ".csv")
+            np.savetxt(result_path, data, delimiter=",", header='radii,kt_mito')
+            print(file_name + " result saved")
+        else:
+            print("No cristae junctions found")
